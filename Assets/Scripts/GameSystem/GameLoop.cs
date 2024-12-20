40281f3a8a9e68a6ce6f808be73ba0add31e0a40
NakayaShutno <ktc2231020001@edu.kyoto-tech.ac.jp>
2024-12-20 19:40:36 +0900

    ゲームのメインシステムの処理を追加 (途中)

--- Assets/Scripts/GameSystem/GameLoop.cs
+++ Assets/Scripts/GameSystem/GameLoop.cs
@@ -2,43 +2,65 @@
 using System.Collections.Generic;
 
 
+//GameLoop 内の処理は外側で UniTask を使う場合を想定して行動に関する処理ごとに分ける。
 public class GameLoop
 {
+    private PlayerIndex[] m_TurnList;
+
     private LinkedList<YokaiInfo> m_Rarity1Pool = new LinkedList<YokaiInfo>();
     private LinkedList<YokaiInfo> m_Rarity2Pool = new LinkedList<YokaiInfo>();
     private LinkedList<YokaiInfo> m_Rarity3Pool = new LinkedList<YokaiInfo>();
 
     private int m_CurrentWeek = 1;
+    private int m_CurrentPlayer = 0;
 
     public LinkedList<YokaiInfo> Rarity1Pool => m_Rarity1Pool;
     public LinkedList<YokaiInfo> Rarity2Pool => m_Rarity2Pool;
     public LinkedList<YokaiInfo> Rarity3Pool => m_Rarity3Pool;
     public int CurrentWeek => m_CurrentWeek;
 
-
-    /// <summary> 順番決め + 星1の手札を順番事に決める </summary>
-    public void Initialize()
+    /// <summary> カードの山札を初期化する </summary>
+    private void SetCardPools()
     {
 
     }
+    // すべてのプレイヤーがサイコロを振る場合はこの中で初期化せず引数から順番を取得する
+    private void SetPlayerTurn(int playerCount)
+    {
+        m_TurnList = new PlayerIndex[playerCount];
 
+    }
+
+
+    /// <summary> 順番決め + 星1の手札を順番事に決める </summary>
+    public void Initialize(int playerCount)
+    {
+        SetCardPools();
+        SetPlayerTurn(playerCount);
+    }
+
+    /// <summary> 通常ターン開始 </summary>
     public void PlayPrepare(PlayerManager player)
     {
 
-
-
-        m_CurrentWeek++;
     }
 
-    /// <summary> 中間講演 </summary>
+    /// <summary> 中間公演 </summary>
     public void PlayInterimPerformance(PlayerManager player)
     {
 
     }
 
-    /// <summary> 最終講演 </summary>
+    /// <summary> 最終公演 </summary>
     public void PlayLastPerformance(PlayerManager player)
     {
 
+    }
+
+    /// <summary> プレイヤーの行動終了後にするターンの更新処理 </summary>
+    public void UpdateTurn()
+    {
+        if (m_CurrentPlayer < m_TurnList.Length) { m_CurrentPlayer++; }
+        else { m_CurrentWeek++; }
     }
 }
--- Assets/Scripts/GameSystem/GameManager.cs
+++ Assets/Scripts/GameSystem/GameManager.cs
@@ -5,7 +5,28 @@ using UnityEngine;
 public static class GameManager
 {
     private static PlayerManager[] m_Players;
+    private static BandGroup[] m_BandGroups;
+    private static GameLoop m_GameLoop;
 
 
+    private static void CreateBandGroups()
+    {
+        m_BandGroups = new BandGroup[m_Players.Length];
+        for(int i = 0; i < m_Players.Length; i++)
+        {
+            m_BandGroups[i] = new BandGroup();
+        }
+    }
+    private static void CreateGameLoop() => m_GameLoop = new GameLoop();
+
+    public static void InitializeData()
+    {
+        CreateBandGroups();
+        CreateGameLoop();
+        m_GameLoop.Initialize(m_Players.Length);
+    }
     public static void SetPlayers(PlayerManager[] players) => m_Players = players;
+
+    public static PlayerManager GetPlayer(int i) => m_Players[i];
+    public static BandGroup GetBandGroup(int i) => m_BandGroups[i];
 }
