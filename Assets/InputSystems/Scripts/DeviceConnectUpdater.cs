6d3e6b41bd3bf5a80ebc56d25f23f20064587a7d
RAYTREK2-75\ktc <KTC23A32A0003@edu.kyoto-tech.ac.jp>
2025-01-24 18:23:12 +0900

    コメントを追加した

--- Assets/InputSystems/Scripts/DeviceConnectUpdater.cs
+++ Assets/InputSystems/Scripts/DeviceConnectUpdater.cs
@@ -22,73 +22,73 @@ public class DeviceConnectUpdater : SingletonBehaviour<DeviceConnectUpdater>
         if (Instance != this) { return; }
         InitPlayerInput();
 
-        InputSystem.onDeviceChange += OnDeviceAdded;
-        InputSystem.onDeviceChange += OnDeviceRemoved;
+        InputSystem.onDeviceChange += OnDeviceAdded;      //デバイスを刺したとき
+        InputSystem.onDeviceChange += OnDeviceRemoved;    //デバイスを抜いたとき
     }
 
-    private void InitPlayerInput()
+    private void InitPlayerInput()                              //PlayerInputの初期化
     {
-        for (int i = 0; i < InputSystem.devices.Count; i++)
+        for (int i = 0; i < InputSystem.devices.Count; i++)     //接続されてるデバイスの数繰り返す
         {
-            CreatePlayerInput(InputSystem.devices[i]);
+            CreatePlayerInput(InputSystem.devices[i]);      //PlayerInputを作成
         }
     }
 
     private void OnDeviceAdded(InputDevice device, InputDeviceChange change)
     {
-        if (change != InputDeviceChange.Added) { return; }
-        CreatePlayerInput(device);
+        if (change != InputDeviceChange.Added) { return; }      //changeがAddedじゃないときはreturn
+        CreatePlayerInput(device);                      // デバイスが追加された場合は、PlayerInputを作成
     }
 
     private void OnDeviceRemoved(InputDevice device, InputDeviceChange change)
     {
-        if (change != InputDeviceChange.Removed) { return; }
+        if (change != InputDeviceChange.Removed) { return; }    //changeがRemovedじゃないときはreturn
 
-        Destroy(m_Observers[device].gameObject);
-        m_Observers.Remove(device);
+        Destroy(m_Observers[device].gameObject);        //m_ObserversをDestroy
+        m_Observers.Remove(device);                     //m_Observerの中のdeviceを削除(参照でおかしくなるから)
     }
 
     private void CreatePlayerInput(InputDevice device)
     {
-        if (m_Observers.ContainsKey(device)) { return; }
-        if (device is Mouse) { return; }
-        if (device is Pen) { return; }
+        if (m_Observers.ContainsKey(device)) { return; }    //すでにこのデバイスが監視されていたらreturn
+        if (device is Mouse) { return; }            //マウスならreturn
+        if (device is Pen) { return; }              //ペンならreturn
 
-        var playerInput = PlayerInput.Instantiate(prefab: prehab.gameObject,
+        var playerInput = PlayerInput.Instantiate(prefab: prehab.gameObject,            //プレハブを配置
                                                   controlScheme: device.GetScheme(),
                                                   pairWithDevice: device);
 
-        playerInput.defaultControlScheme = device.GetScheme();
-        m_Observers.Add(device, playerInput.GetComponent<PlayerInputObserver>());
-        playerInput.transform.SetParent(transform);
+        playerInput.defaultControlScheme = device.GetScheme();          //デバイスの種類を格納する
+        m_Observers.Add(device, playerInput.GetComponent<PlayerInputObserver>());       //m_ObserverのなかにdeviceとPlayerInputObserverを格納
+        playerInput.transform.SetParent(transform);             //PlayerInputをこのオブジェクトの子オブジェクトにする
     }
 
     private void OnDestroy()
     {
-        InputSystem.onDeviceChange -= OnDeviceAdded;
-        InputSystem.onDeviceChange -= OnDeviceRemoved;
+        InputSystem.onDeviceChange -= OnDeviceAdded;        // デバイスが追加されたときのイベントを解除
+        InputSystem.onDeviceChange -= OnDeviceRemoved;      // デバイスが削除されたときのイベントを解除
     }
 
 
-    public PlayerInputObserver[] GetObserversByButtonDown(string key)
-        => m_Observers.Select(pair => pair.Value)
-                      .Where(observer => observer.GetButtonDown(key))
-                      .ToArray();
+    public PlayerInputObserver[] GetObserversByButtonDown(string key)       // m_Observers辞書から各PlayerInputObserverを取り出し、ボタンが押されたものだけをフィルタリング
+        => m_Observers.Select(pair => pair.Value)       // 辞書の各要素からValue（PlayerInputObserver）を取得
+                      .Where(observer => observer.GetButtonDown(key))       // その中から指定したボタンが押されたPlayerInputObserverだけをフィルタリング
+                      .ToArray();       // 最終的にフィルタリングされたPlayerInputObserverを配列にして返す
 
     public PlayerInputObserver[] GetPlayerInputObserverAs<T>() where T : InputDevice
-        => Observers?.Where(receiver => receiver.Value.Device is T)?.Select(receiver => receiver.Value)?.ToArray();
+        => Observers?.Where(receiver => receiver.Value.Device is T)?.Select(receiver => receiver.Value)?.ToArray();     //メモで図解
 }
 
 public static class InputDeviceExtension
 {
     public static string GetScheme<T>(this T Scheme) where T : InputDevice
     {
-        return (Scheme) switch
+        return (Scheme) switch              // Schemeの型によって分岐するswitch式
         {
-            Keyboard => "Keyboard&Mouse",
-            Gamepad => "Gamepad",
-            Touchscreen => "Touch",
-            _ => ""
+            Keyboard => "Keyboard&Mouse",   // キーボードの場合は"Keyboard&Mouse"
+            Gamepad => "Gamepad",           // ゲームパッドの場合は"Gamepad"
+            Touchscreen => "Touch",         // タッチスクリーンの場合は"Touch"
+            _ => ""                         // それ以外の場合は空文字を返す
         };
     }
 }
